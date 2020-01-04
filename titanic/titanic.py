1a3322ccd408922f7aeaa80c225d85fa5569205a
Hiroyuki Tanaka <ht0919@gmail.com>
2020-01-04 14:43:37 +0900

    Add titanic ver.03

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,26 +1,18 @@
-from sklearn.ensemble import GradientBoostingClassifier
 import pandas as pd
 import numpy as np
 
-#train = pd.read_csv("/kaggle/input/titanic/train.csv")
-#test = pd.read_csv("/kaggle/input/titanic/test.csv")
 train = pd.read_csv("./data/train.csv")
 test = pd.read_csv("./data/test.csv")
 
-# 前処理：欠損データの補完
-# 代理データを中央値や最頻値で置き換える
 train["Age"] = train["Age"].fillna(train["Age"].median())
 train["Embarked"] = train["Embarked"].fillna("S")
 
-# 前処理：カテゴリカルデータの数値化
-# Sexは['male','female']を[0,1]に、Embarkedは['S','C','Q']を[0,1,2]に変換
 train["Sex"].loc[train["Sex"] == "male"] = 0
 train["Sex"].loc[train["Sex"] == "female"] = 1
-train["Embarked"].loc[train["Embarked"] == "S"] = 0
-train["Embarked"].loc[train["Embarked"] == "C"] = 1
+train["Embarked"].loc[train["Embarked"] == "S" ] = 0
+train["Embarked"].loc[train["Embarked"] == "C" ] = 1
 train["Embarked"].loc[train["Embarked"] == "Q"] = 2
 
-# testもtrainと同様に前処理
 test["Age"] = test["Age"].fillna(test["Age"].median())
 test["Sex"].loc[test["Sex"] == "male"] = 0
 test["Sex"].loc[test["Sex"] == "female"] = 1
@@ -29,26 +21,22 @@ test["Embarked"].loc[test["Embarked"] == "C"] = 1
 test["Embarked"].loc[test["Embarked"] == "Q"] = 2
 test.Fare.loc[152] = test.Fare.median()
 
-#　家族数を追加
-train_two = train.copy()
-train_two["family_size"] = train_two["SibSp"] + train_two["Parch"] + 1
-test_two = test.copy()
-test_two["family_size"] = test_two["SibSp"] + test_two["Parch"] + 1
+# scikit-learnのインポートをします
+from sklearn import tree
 
 # 「train」の目的変数と説明変数の値を取得
 target = train["Survived"].values
-features = train_two[["Pclass", "Age", "Sex", "Fare", "family_size", "Embarked"]].values
-
-# モデルは勾配ブースティング
-forest = GradientBoostingClassifier(n_estimators=55, random_state=9)
-forest = forest.fit(features, target)
-
-# testから値を取り出す
-test_features = test_two[["Pclass", "Age", "Sex", "Fare", "family_size", "Embarked"]].values
-
-# 予測をしてCSVへ書き出す
-my_prediction_forest = forest.predict(test_features)
+# 追加となった項目も含めて予測モデルその2で使う値を取り出す
+features_two = train[["Pclass","Age","Sex","Fare", "SibSp", "Parch", "Embarked"]].values
+# 決定木の作成とアーギュメントの設定
+max_depth = 10
+min_samples_split = 5
+my_tree_two = tree.DecisionTreeClassifier(max_depth = max_depth, min_samples_split = min_samples_split, random_state = 1)
+my_tree_two = my_tree_two.fit(features_two, target)
+# tsetから「その2」で使う項目の値を取り出す
+test_features_2 = test[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
+# 「その2」の決定木を使って予測をしてCSVへ書き出す
+my_prediction_tree_two = my_tree_two.predict(test_features_2)
 PassengerId = np.array(test["PassengerId"]).astype(int)
-my_solution_forest = pd.DataFrame(my_prediction_forest, PassengerId, columns=["Survived"])
-#my_solution_forest.to_csv("/kaggle/working/gender_submission.csv", index_label=["PassengerId"])
-my_solution_forest.to_csv("./data/submit.csv", index_label=["PassengerId"])
+my_solution_tree_two = pd.DataFrame(my_prediction_tree_two, PassengerId, columns = ["Survived"])
+my_solution_tree_two.to_csv("./data/submit.csv", index_label = ["PassengerId"])
