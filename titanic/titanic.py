05626057f94f9488b5214b111cbb8d39e99f38fb
Hiroyuki Tanaka <ht0919@gmail.com>
2020-01-04 14:50:33 +0900

    Add titanic ver.05

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -2,30 +2,24 @@ from sklearn.ensemble import GradientBoostingClassifier
 import pandas as pd
 import numpy as np
 
+#train = pd.read_csv("/kaggle/input/titanic/train.csv")
+#test = pd.read_csv("/kaggle/input/titanic/test.csv")
 train = pd.read_csv("./data/train.csv")
 test = pd.read_csv("./data/test.csv")
 
 # 前処理：欠損データの補完
-# 代理データを中央値や最頻値で置き換える
+# 欠損データを中央値や最頻値で置き換える
 train["Age"] = train["Age"].fillna(train["Age"].median())
 train["Embarked"] = train["Embarked"].fillna("S")
-
 # 前処理：カテゴリカルデータの数値化
 # Sexは['male','female']を[0,1]に、Embarkedは['S','C','Q']を[0,1,2]に変換
-train["Sex"].loc[train["Sex"] == "male"] = 0
-train["Sex"].loc[train["Sex"] == "female"] = 1
-train["Embarked"].loc[train["Embarked"] == "S"] = 0
-train["Embarked"].loc[train["Embarked"] == "C"] = 1
-train["Embarked"].loc[train["Embarked"] == "Q"] = 2
-
+train["Sex"] = train.Sex.replace("male",0).replace("female",1)
+train["Embarked"] = train.Embarked.replace("S",0).replace("C",1).replace("Q",2)
 # testもtrainと同様に前処理
 test["Age"] = test["Age"].fillna(test["Age"].median())
-test["Sex"].loc[test["Sex"] == "male"] = 0
-test["Sex"].loc[test["Sex"] == "female"] = 1
-test["Embarked"].loc[test["Embarked"] == "S"] = 0
-test["Embarked"].loc[test["Embarked"] == "C"] = 1
-test["Embarked"].loc[test["Embarked"] == "Q"] = 2
-test.Fare.loc[152] = test.Fare.median()
+test["Fare"] = test["Fare"].fillna(test["Fare"].median())
+test["Sex"] = test.Sex.replace("male",0).replace("female",1)
+test["Embarked"] = test.Embarked.replace("S",0).replace("C",1).replace("Q",2)
 
 #　家族数を追加
 train_two = train.copy()
@@ -48,4 +42,5 @@ test_features = test_two[["Pclass", "Age", "Sex", "Fare", "family_size", "Embark
 my_prediction_forest = forest.predict(test_features)
 PassengerId = np.array(test["PassengerId"]).astype(int)
 my_solution_forest = pd.DataFrame(my_prediction_forest, PassengerId, columns=["Survived"])
+#my_solution_forest.to_csv("/kaggle/working/gender_submission.csv", index_label=["PassengerId"])
 my_solution_forest.to_csv("./data/submit.csv", index_label=["PassengerId"])
